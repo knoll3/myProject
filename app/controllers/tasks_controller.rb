bb566405bcfbf7eabb9ca7290a1e3e6531c253d6
Kyle <noll.kyle@gmail.com>
2018-12-22 15:27:06 -0600

    Add status to task migration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,6 @@ class TasksController < ApplicationController
 	private
 
 		def task_params
-			params.require(:task).permit(:name, :description)
+			params.require(:task).permit(:name, :description, :status)
 		end
 end
