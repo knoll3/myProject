e0b8bc76bac2e9972f90a6fff525af7c2d11568e
Kyle <noll.kyle@gmail.com>
2018-12-22 18:41:13 -0600

    Set up relationship between tasks and goals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_212513) do
+ActiveRecord::Schema.define(version: 2018_12_23_003623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2018_12_22_212513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "goal_id"
+    t.index ["goal_id"], name: "index_tasks_on_goal_id"
   end
 
 end
